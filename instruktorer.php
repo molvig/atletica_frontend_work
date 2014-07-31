789adec9efc4b9b2e452c216de854e15147affa2
Jennie Molvig <jennie@molvig.se>
2014-07-31 12:28:21 +0200

    torsdag fm

---
+++ instruktorer.php
@@ -0,0 +1,92 @@
+<?php include("inc/header.php"); ?>
+
+<div class="grid_12">
+	<div class="inst_1">
+
+		<div class="instruktor">
+			<div class="inst">
+				<img src="img/personal/sandra.jpg" alt="..." class="img-rounded">
+			</div>	
+
+			<div class="inst">
+				<p>Namn: Sandra <br>
+				Motto: Strong is the new skinny</p>
+			</div>	
+		</div>	
+	</div>
+
+	<div class="inst_1">
+
+		<div class="instruktor">
+			<div class="inst">
+				<img src="img/personal/amilia.jpg" alt="..." class="img-rounded">
+			</div>	
+
+			<div class="inst">
+				<p>Namn: Amilia <br>
+				Motto: Strong is the new skinny</p>
+			</div>	
+		</div>	
+	</div>
+
+	<div class="inst_1">
+
+		<div class="instruktor">
+			<div class="inst">
+				<img src="img/personal/ellinor.jpg" alt="..." class="img-rounded">
+			</div>	
+
+			<div class="inst">
+				<p>Namn: Ellinor <br>
+				Motto: Strong is the new skinny</p>
+			</div>	
+		</div>	
+	</div>
+
+	<div class="inst_1">
+
+		<div class="instruktor">
+			<div class="inst">
+				<img src="img/personal/conny.jpg" alt="..." class="img-rounded">
+			</div>	
+
+			<div class="inst">
+				<p>Namn: Conny <br>
+				Motto: Strong is the new skinny</p>
+			</div>	
+		</div>	
+	</div>
+
+	<div class="inst_1">
+
+		<div class="instruktor">
+			<div class="inst">
+				<img src="img/personal/ulle.jpg" alt="..." class="img-rounded">
+			</div>	
+
+			<div class="inst">
+				<p>Namn: Ulle <br>
+				Motto: Strong is the new skinny</p>
+			</div>	
+		</div>	
+	</div>
+
+	<div class="inst_1">
+
+		<div class="instruktor">
+			<div class="inst">
+				<img src="img/personal/martinb.jpg" alt="..." class="img-rounded">
+			</div>	
+
+			<div class="inst">
+				<p>Namn: Martin <br>
+				Motto: Strong is the new skinny</p>
+			</div>	
+		</div>	
+	</div>
+</div>
+
+
+
+
+<?php include("inc/footer.php"); ?>
