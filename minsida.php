a7e837245828b532909741bf951c43f24f6ae6c9
jennie@molvig.se <molvig>
2014-08-22 20:50:27 +0200

    fredag 20.50

--- minsida.php
+++ minsida.php
@@ -1,11 +1,17 @@
 <?php include('inc/header.php'); ?>
+<div class="grid_12">
+<div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-thumbs-up"></span> Du har X dagar kvar på ditt medlemskap</div>
+	<div class="alert alert-warning" role="alert"><span class="glyphicon glyphicon-thumbs-up"></span> Du har bara 7 dagar kvar på ditt medlemskap</div>
+	<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-warning-sign"></span> Ditt medlemsskap har gått ut. Kontakta Atletica om du vill förnya.</div> 
+</div>
 	<div class="grid_3">
   <?php include("inc/menymedlem.php"); ?> 
   </div>
 
+
   <div class="grid_6">
-    <h3> Hej, Jennie</h3>
-      <h4>Du har <strong>2</strong> gruppass bokade</h4>
+    <h3> Hej, X</h3>
+      <h4>Du har <strong>X</strong> gruppass bokade</h4>
   </div>
 
 
